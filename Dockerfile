f66a9f2e14a71fcb2c311e9c3cba8a4b95d4aa37
Kalle Ahlström <hashtagkalski@gmail.com>
2022-08-25 12:28:20 +0300

    add a learning model to a day's 'beautifulness'

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # syntax=docker/dockerfile:1
-FROM python:3.10-alpine as base
+FROM python:3.10-slim-buster as base
 
 # Setup ENV variables here (if needed in the future)
 
@@ -14,7 +14,6 @@ COPY Pipfile .
 COPY Pipfile.lock .
 RUN PIPENV_VENV_IN_PROJECT=1 pipenv install --deploy
 
-
 FROM base as runtime
 
 # Copy virtual env from python-deps stage
@@ -26,6 +25,8 @@ WORKDIR /bot
 
 # Install app into container
 COPY . .
+#generate prisma
+RUN prisma generate
 
 # Create a non-root user and add permission to access /bot folder
 RUN adduser -u 5678 --disabled-password --gecos "" botuser
--- bot.py
+++ bot.py
@@ -1,16 +1,23 @@
+from datetime import datetime, timedelta
 import os
-from random import random
+from typing import Tuple
+from prisma import Prisma
+from prisma.models import WeatherIcon
+from random import choices, random
 import re
 import requests
 from dotenv import load_dotenv
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
+from telegram.constants import ChatType
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, PollHandler, filters
 load_dotenv()
-
+POLL_TIME = timedelta(minutes=10)
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 WEATHER_API = os.getenv('WEATHER_API')
 if not BOT_TOKEN or not WEATHER_API:
     print("env variables missing")
+weather_code_dict: dict[str, Tuple[WeatherIcon, datetime]] = {}
+poll_created_dict: dict[int, Tuple[int, datetime]] = {}
 
 filter = ['päivä', 'day', 'sää', 'keli', 'weather', 'ulkona',
           'kylmä', 'lämmin', 'sataa', 'sade', 'lunta', 'lumi']
@@ -20,53 +27,155 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
 
-async def day(update: Update, context: ContextTypes.DEFAULT_TYPE):
+def poll_expired(created_at: datetime):
+    return datetime.now()-created_at < POLL_TIME
+
+
+def fetch_weather():
     weather = requests.get(
         f"https://api.openweathermap.org/data/2.5/weather?q=otaniemi&appid={WEATHER_API}&units=metric&lang=fi")
     weather = weather.json()
+    return weather
+
+
+async def day(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    weather = fetch_weather()
     descriptives = weather['weather'][0]
     weather_desc = descriptives['description']
     code = descriptives['id']
     measurements = weather['main']
-    temp = measurements['temp']
+    temp = float(measurements['temp'])
+    temp_rounded = int(5*(temp//5))
     moist = measurements['humidity']
-    beautiful_pct=0
-
-    #https://openweathermap.org/weather-conditions
-    match code//100:
-        case 8: #clouds
-            match code%100:
-                case 0:
-                    beautiful_pct=100# clear sky
-                case 1:
-                    beautiful_pct=75+random()*11  # 11-25% cloudy
-                case 2:
-                    beautiful_pct=50+random()*25#25-50% cloudy
-                case 3:
-                    beautiful_pct=16+random()*33#51-84% broken
-                case 4:
-                    beautiful_pct=random()*15
-            pass
-        case 6:#snow
-            
-            pass
-        case 5:#rain
-            pass
-        case 3:#drizzle
-            pass
+    if code in weather_code_dict:
+        (res, timestamp) = weather_code_dict[code]
+        if (datetime.now()-timestamp).total_seconds() > 300:
+            res, timestamp = await fetch_from_db(code, temp_rounded)
+    else:
+        res, timestamp = await fetch_from_db(code, temp_rounded)
+    if res:
+        weather_code_dict[code] = (res, timestamp)
+        beautiful_pct = res.votes_yes/(res.votes_no+res.votes_yes)*100
+        beautiness = 'Kaunis' if beautiful_pct > 50 else 'Ei kaunis'
+    else:
+        beautiful_pct = '??'
+        beautiness = ""
+    await update.effective_chat.send_message(f"{weather_desc}: {temp:.1f} °C, {moist} % 💦\nPäivä: {beautiness} ({beautiful_pct:.2f})%")
 
 
-        
+async def start_poll(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat = update.effective_chat
+    if chat.id in poll_created_dict:
+        poll_created = poll_created_dict[chat.id][1]
+        if poll_expired(poll_created):
+            return await update.message.reply_text("Äänestys on jo käynnissä!\nA vote is already in progress!")
+    created_poll = await chat.send_poll('Onko tänään kaunis päivä?\n Is it beautiful day today?', ['Kyllä/Yes', 'Ei/No'], close_date=(datetime.now()+POLL_TIME))
+    poll_created_dict[chat.id] = (created_poll.id, datetime.now())
 
-    await update.effective_chat.send_message(f"{weather_desc}: {temp:.1f} °C, {moist} % 💦\nPäivä: {'Kaunis' if beautiful_pct>50 else 'Ei kaunis'} ({beautiful_pct:.2f})%")
+
+async def close_poll(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.effective_chat.type != ChatType.PRIVATE:
+        admins = await update.effective_chat.get_administrators()
+        if update.effective_user.id not in [admin.user.id for admin in admins]:
+            return await update.effective_message.reply_text("Ei oikeuksia\nPermissions denied")
+    chat_id = update.effective_chat.id
+    if chat_id not in poll_created_dict or not poll_expired(poll_created_dict[chat_id][1]):
+        print("lol")
+        return await update.effective_chat.send_message("Ei aktiivista äänestystä chatissa!\nNo active poll in chat")
+    poll_id = poll_created_dict[chat_id][0]
+    await context.bot.stop_poll(chat_id, poll_id)
+    del poll_created_dict[chat_id]
+    await update.effective_chat.send_message("Äänestys suljettu\nPoll closed.")
+
+
+async def handle_poll_ended(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if not update.poll.is_closed:
+        return
+    choices = update.poll.options
+    yes_amount = [
+        option.voter_count for option in choices if "Kyllä" in option.text][0]
+    no_amount = [
+        option.voter_count for option in choices if "Ei" in option.text][0]
+    weather = fetch_weather()
+    code = weather['weather'][0]['id']
+    temp = float(weather['main']['temp'])
+    temp_rounded = int(5*(temp//5))
+    weather = await add_data_to_db(code, temp_rounded, yes_amount, no_amount)
+    if not weather:
+        del weather_code_dict[code]
+    else:
+        weather_code_dict[code] = weather, datetime.now()
+
+
+async def add_data_to_db(code: int, temp_rounded: int, yes_amount: int, no_amount: int):
+    db = Prisma()
+    try:
+        await db.connect()
+        res = await db.weathericon.find_first(where={
+            'code': code,
+            'temperature': temp_rounded
+        }
+        )
+        if not res:
+            res = await db.weathericon.create(data={
+                'code': code,
+                'temperature': temp_rounded,
+                'votes_yes': yes_amount,
+                'votes_no': no_amount
+            })
+        else:
+            res = await db.weathericon.update(where={
+                'id': res.id
+            }, data={
+                'votes_yes': {
+                    'increment': yes_amount
+                },
+                'votes_no': {
+                    'increment': no_amount
+                }
+            })
+    except Exception as e:
+        print(e)
+        return None
+    finally:
+        if db.is_connected():
+            await db.disconnect()
+    return res
+
+
+async def fetch_from_db(code: str, temp_rounded: int):
+    db = Prisma()
+    try:
+        await db.connect()
+        db_res = await db.weathericon.find_first(
+            where={
+                'code': code,
+                'temperature': temp_rounded
+            }
+        )
+        if not db_res:
+            db_res = await db.weathericon.create({
+                'code': code,
+                'temperature': temp_rounded
+            })
+    except Exception as e:
+        print(e)
+        return None, datetime.now()-timedelta(minutes=10)
+    finally:
+        if db.is_connected():
+            await db.disconnect()
+    return db_res, datetime.now()
+
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(BOT_TOKEN).build()
     start_handler = CommandHandler('start', start)
-    application.add_handler(start_handler)
     day_handler = CommandHandler('paiva', day)
-    application.add_handler(day_handler)
+    vote_handler = CommandHandler(['vote', 'aanesta'], start_poll)
+    close_handler = CommandHandler(['close', 'sulje'], close_poll)
+    poll_hander = PollHandler(handle_poll_ended)
     message_handler = MessageHandler(filters=(
         filters.CHAT & filters.Regex(re.compile('|'.join(filter), re.IGNORECASE))), callback=day)
-    application.add_handler(message_handler)
+    application.add_handlers(
+        [start_handler, vote_handler, day_handler, poll_hander, close_handler, message_handler])
     application.run_polling()
